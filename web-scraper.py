55aa17d989a45124ba3fe4df6b392534ed613173
Iris Leal <iris_leal_m@hotmail.com>
2020-06-02 16:49:48 -0700

    Now it took a paragraph and realice if contain a word

--- web-scraper.py
+++ web-scraper.py
@@ -1,28 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 
-# send a request to the URL
-# URL = 'https://en.wikipedia.org/wiki/History_of_Mexico'
-# response = requests.get(URL)
-
-# print(response)
-# Extract the content
-# content = response.content
-
-# # convert ro BS object
-# soup = BeautifulSoup(content,'html.parser')
-# print(soup)
-
-# Find the main element
-# results = soup.find(id='mw-content-text')
-
-# find all the citation
-# citations = results.find_all('sup', class_='noprint Inline-Template Template-Fact')
-
-# <sup class="noprint Inline-Template Template-Fact" style="white-space:nowrap;">[<i><a href="/wiki/Wikipedia:Citation_needed" title="Wikipedia:Citation needed"><span title="This claim needs references to reliable sources. (August 2010)">citation needed</span></a></i>]</sup>
-# # Obtainn the number of citations
-# num_citations = len(citations)
-# print(num_citations)
 
 def get_citations_needed_count(URL):
     num_citations = 0
@@ -42,7 +20,42 @@ def get_citations_needed_count(URL):
     return f'The number of citations in this URL are: {str(num_citations)}.'
 
 
+def get_citations_needed_report(URL):
+    passages = ''
+    response = requests.get(URL)
+
+    content = response.content
+    soup = BeautifulSoup(content,'html.parser')    
+    main_elements = soup.find(id='mw-content-text')
+    all_paragraphs = main_elements.find_all('p')
+    
+    
+    # print(all_paragraphs[0:1])
+    paragraph = str(all_paragraphs[0])
+    print('***********')
+    print (paragraph)
+    print('***********')
+    # paragraph = all_paragraphs[1]
+    # print('***********')
+    # print (paragraph)
+    # print('***********')
+#     txt = "The rain in Spain stays mainly in the plain"
+# x = "ain" in txt
+# print(x)
+    if 'iris' in paragraph:
+        print('yeap')
+    else:
+        print('nop')
+
+    # print(len(all_paragraphs))
+    # for parag
+
+
+    return passages
+
+
 
 if __name__ == "__main__":
     URL = 'https://en.wikipedia.org/wiki/History_of_Mexico' 
-    print(get_citations_needed_count(URL))
+    # print(get_citations_needed_count(URL))
+    print(get_citations_needed_report(URL))
